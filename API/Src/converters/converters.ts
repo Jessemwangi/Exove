da22bac4a20133f8c1eaec6287c8b9973938d26a
Anna Petelin <andorjamb@protonmail.com>
2023-04-30 12:32:01 +0300

    add example template converter

--- API/Src/converters/converters.ts
+++
@@ -1,6 +0,0 @@
-/** For creating conversions from mongoose json objects to json objects matching frontend data requirements */
-import { ITemplates } from "../dbcontext/Interfaces.js";
-
-export const templateConverter = (template:ITemplates)=>{
-    
-}
