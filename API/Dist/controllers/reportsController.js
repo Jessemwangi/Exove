6581bc253252660c5c828aadf9815419dbacdbe4
jessemwangi <jessejzee@gmail.com>
2023-05-16 12:20:48 +0300

    fix bug in getting reports

--- API/Dist/controllers/reportsController.js
+++ API/Dist/controllers/reportsController.js
@@ -5,7 +5,7 @@ import { savedSuccess } from "../Configs/serverConfig.js";
 export const getReports = async (req, res, next) => {
     try {
         await dbconnect();
-        const reportsData = Reports.find({}).lean();
+        const reportsData = Reports.find({}).select('-__v').lean();
         await dbclose();
         res.status(200).json(reportsData);
     }
